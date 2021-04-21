5364727bbc53197cd15e97ea2163ec901ec9c900
houko <ko.h@ctw.inc>
2021-04-21 16:10:41 +0900

    add scss support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const path = require('path')
 module.exports = {
     future: {
         webpack5: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
     },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
