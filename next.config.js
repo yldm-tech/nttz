14b545182723be21b77f13e13c84d22ae351ac01
houko <ko.h@ctw.inc>
2021-04-22 16:42:59 +0900

    build dir

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-    distDir:'out',
+    distDir: 'out',
     future: {
         webpack5: true,
     },
