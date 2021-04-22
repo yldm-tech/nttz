b02878090e7e5ad7bb1454e49094a6b9b329aa51
houko <ko.h@ctw.inc>
2021-04-22 17:40:09 +0900

    添加生命周期示例

---
+++ pages/life.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { NextPageContext } from 'next'
+
+interface Props {
+    userAgent?: string;
+}
+
+// 绿色:仅客户端    黄色：都执行     红色：仅服务器
+// 生命周期示例，顺便打了下userAgent,重点关注浏览器console和IDE的console内容
+export default class extends Component<Props> {
+    static async getInitialProps({req}: NextPageContext) {   //s1
+        console.log('getInitialProps');
+        const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+        return {userAgent};
+    }
+
+    constructor(props: Props) { //s2   //c1
+        super(props);
+        console.log('constructor');
+    }
+
+    UNSAFE_componentWillMount() { //s3   //c2
+        console.log('UNSAFE_componentWillMount');
+    }
+
+    render() {  //s4    //c3
+        console.log('render');
+        return (
+            <div>
+                Hello World {this.props.userAgent}
+            </div>
+        );
+    }
+
+    componentDidMount() {  //c4
+        console.log('componentDidMount');
+    }
+
+}
